153034bfcb07a2dd4005d9aabb86b9d2e09f51d3
Adil <adil@adil.com>
2022-05-22 21:26:01 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# openviduioslib
+
+A description of this package.
---
+++ Tests/openviduioslibTests/openviduioslibTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import openviduioslib
+
+final class openviduioslibTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(openviduioslib().text, "Hello, World!")
+    }
+}
